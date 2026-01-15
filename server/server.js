0b4c0d96013d8c84ac323842dc9e412b37bff849
NedasU <nedasurba@gmail.com>
2026-01-15 18:36:47 +0200

    Added API get requests to get city name and make a call to geoapify and its geocode API to get its place_id and other useful information. Performing checks and data filtration to only return the needed data. API places get request takes in city_id as a required query and 3 additional non-neccessary queries. Makes a call to geoapify's places API to return touristic places around the area based on the users entered city. filters data and returns only the required data. Additionally, added a placeholder image in the case of missing images (quite common) from Places API

---
+++ server/server.js
@@ -0,0 +1,100 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import path from "path";
+import { fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json())
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.get("/", (req, res) => {
+    res.send("Server running");
+});
+
+app.get("/api/geocode", async (req, res) => {
+    const { city } = req.query;
+
+    if (!city) {
+        return res.status(400).json({ error: "City is Required!" });
+    }
+
+    try {
+        const response  = await fetch(
+            `https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(
+                city
+            )}&apiKey=${process.env.GEOAPIFY_KEY}`
+        );
+    
+        if (!response.ok) {
+            throw new Error("Geoapify request failed!");
+        }
+
+    const data = await response.json();
+    const feature = data.features[0];
+
+    if (!feature) {
+      return res.status(404).json({ error: "City not found" });
+    }
+
+    const { lat, lon, city: cityName, country, place_id } = feature.properties;
+    const { bbox } = feature;
+
+    res.json({
+      city: cityName,
+      country,
+      lat,
+      lon,
+      bbox,
+      place_id
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Geocoding failed" });
+  }
+});
+
+app.get("/api/places", async (req, res) => {
+    const { city_id, category = "tourism", limit = 40, offset = 0 } = req.query;
+
+    if (!city_id){
+        return res.status(400).json({ error: "Invalid place id!" })
+    }
+    try {
+        const response = await fetch(`https://api.geoapify.com/v2/places?categories=${category}&filter=place:${city_id}&limit=${limit}&apiKey=${process.env.GEOAPIFY_KEY}&offset=${offset}`)
+
+        if (!response.ok) {
+            return res.status(400).json({ error: "Didnt find any places!" })
+        }
+        
+        const data = await response.json();
+
+        const places = data.features.map((place_info) => ({
+                name: place_info.properties.name || "Unnamed place",
+                lon: place_info.properties.lon,
+                lat: place_info.properties.lat,
+                address: place_info.properties.formatted,
+                categories: place_info.properties.categories ?? [],
+                place_id: place_info.properties.place_id,
+                img_src: place_info.properties.wiki_and_media?.image ?? "/images/placeholder.jpg"
+
+        }));
+        res.json({ places });
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ error: "Error occured whilst searching for places!" })
+    }
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
